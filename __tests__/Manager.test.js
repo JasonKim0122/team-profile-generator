10b98aa77c80a92cf26eccbab6a3ff7a899c6145
JasonKim0122 <JasonKim0122@gmail.com>
2022-05-07 18:17:38 -0500

    added manager object and tested methods

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,20 @@
-const Manager = require('../lib/Manager')
+const Manager = require('../lib/Manager');
+
+test('Creating new Manager objects', () => {
+    const employee = new Manager('Dave', 1, 'dave@test.com', 5);
+
+    expect(employee.name).toBe('Dave');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    expect(employee.officeNumber).toEqual(expect.any(Number));
+});
+
+test('Testing new methods', () => {
+    const employee = new Manager('Dave', 1, 'dave@test.com', 5);
+
+    expect(employee.getName()).toBe('Dave');
+    expect(employee.getId()).toBe(employee.id);
+    expect(employee.getEmail()).toBe(employee.email);
+    expect(employee.getOfficeNumber()).toBe(employee.officeNumber);
+    expect(employee.getRole()).toBe('Manager');
+});
